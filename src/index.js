671ed133b9e8ac375ba7a50340d60e6650515530
jiang1991 <jiang12196@126.com>
2017-08-25 22:59:13 +0800

    add css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import WrappedNormalLoginForm from './component/login/login';
-import registerServiceWorker from './registerServiceWorker';
 import {render, Router, Route} from 'mirrorx';
 // eslint-disable-next-line
 import mirror from './data/model/app';
@@ -16,4 +15,4 @@ render(
     </Router>,
     document.getElementById('root')
 );
-registerServiceWorker();
+// registerServiceWorker();
